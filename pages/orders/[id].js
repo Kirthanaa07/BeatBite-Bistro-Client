e6ed056cb4b6681815e14228cf984f037a0802b0
Kirthana Sukumaran <kirthanaa07@gmail.com>
2024-01-16 19:34:11 -0600

    import

--- pages/orders/[id].js
+++ pages/orders/[id].js
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from 'next/router';
-import { getSingleOrder } from '../../utils/data/orderData';
 import { Button } from 'react-bootstrap';
+import { getSingleOrder } from '../../utils/data/orderData';
 
 function SingleOrder() {
   const [singleOrderDetails, setSingleOrderDetails] = useState({ items: [] });
